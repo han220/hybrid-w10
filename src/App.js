5a303f60dbb8729e6517736ce5f5645cb4dc4faa
junglesub <junglesubmarine@gmail.com>
2024-11-09 22:25:49 +0900

    fin

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+import myImage from "./me.png";
 
 function App() {
+  const [myMessage, setMyMessage] = useState(
+    localStorage.getItem("myData") || "반갑습니다!"
+  );
+  useEffect(() => localStorage.setItem("myData", myMessage), [myMessage]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={myImage} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          제 이름은 유정섭입니다. <br />
+          컴퓨터 공학 심화를 전공하고 있어요.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <p>
+          저에 대한 여러분들의 노트를 남겨보세요!
+          <br />* 브라우저 저장소에 저장됩니다.
+        </p>
+        <div
+          style={{
+            maxWidth: 500,
+            width: "100%",
+            padding: "16px 32px",
+            boxSizing: "border-box",
+          }}
         >
-          Learn React
-        </a>
+          <textarea
+            style={{
+              width: "100%",
+            }}
+            value={myMessage}
+            onChange={(e) => setMyMessage(e.target.value)}
+          />
+        </div>
       </header>
     </div>
   );
